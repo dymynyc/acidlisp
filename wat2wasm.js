c458f570aceaba43bf242d7d7da0847ccef6a92d
Dominic Tarr <dominic.tarr@gmail.com>
2020-04-05 16:00:21 +1200

    compile wasm by shelling out to wat2wasm

---
+++ wat2wasm.js
@@ -0,0 +1,20 @@
+var fs = require('fs')
+var cp = require('child_process')
+
+function load(file) {
+  var wasm = fs.readFileSync(file)
+  var m = new WebAssembly.Module(wasm)
+  var instance = new WebAssembly.Instance(m)
+  if(instance.exports.main)
+    return instance.exports.main
+  else
+    returninstance.exports
+}
+module.exports = function (src, cb) {
+  var ts = Date.now()
+  var fn_in = '/tmp/wat2wasm_'+ts+'.wat'
+  var fn_out = '/tmp/wat2wasm_'+ts+'.wasm'
+  fs.writeFileSync(fn_in, src)
+  cp.execSync('wat2wasm '+fn_in+' -o '+fn_out)
+  return load(fn_out)
+}

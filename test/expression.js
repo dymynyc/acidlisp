7671a3db50962458fc482ab5fb1340554a5a9432
Dominic Tarr <dominic.tarr@gmail.com>
2020-04-06 22:15:38 +1200

    tests converting expressions to statements! ugly but works!

--- test/expression.js
+++ test/expression.js
@@ -1,32 +1,34 @@
 var tape = require('tape')
-var {flatten, stringify} = require('../util')
+var {flatten, stringify, isNumber} = require('../util')
 var parse = require('../parse')
 var ev = require('../eval')
-var env = {a: 7, b: 0, d: 0, e: 0, f: 1, odd: ([a]) => a & 1, add: ([a, b])=> a+b }
+var env = {a: 7, b: 0, d: 0, e: 0, f: 1, and: ([a]) => a & 1, add: ([a, b])=> a+b }
+var compileWat = require('../compile/wat')
+var wat2wasm = require('../wat2wasm')
 
 var inputs = [
   '(add (if a 1 2) 3)',
   '(add 1 2)',
-  '(if (odd a) 0 1)',
+  '(if (and a 1) 0 1)',
   // (and a b) ;; except that then/else block is a statement so can hold another if
-  '(if a (if b true false) false)',
+  '(if a (if b 1 -1) -10)',
     //(or d e f)
-  '(if d true (if e true (if f true false) false) false)',
+  '(if d 1 (if e 10 (if f 100 -1) -10) -100)',
 ]
 
 var outputs = [
   '(block (if a (def $1 1) (def $1 2)) (def $2 (add $1 3)))',
   '(block (def $1 (add 1 2)))',
-  '(block (def $1 (odd a)) (block (if $1 (def $2 0) (def $2 1)) $2))',
-  '(block (if a (block (if b (def $2 true) (def $2 false)) (def $1 $2)) (def $1 false)) $1)'
+  '(block (def $1 (and a 1)) (block (if $1 (def $2 0) (def $2 1)) $2))',
+  '(block (if a (block (if b (def $2 100) (def $2 -1)) (def $1 $2)) (def $1 -1)) $1)'
 ]
 
 var values = [
   4,
   3,
   0,
-  false,
-  true
+  -1,
+  100
 ]
 
 /*
@@ -56,6 +58,15 @@ inputs.forEach(function (_, i) {
     //t.equal(stringify(), outputs[i])
     t.equal(ev(ast, {__proto__: env}), values[i])
     t.equal(ev(flat_ast, {__proto__: env}), values[i])
+    var wat = compileWat([Symbol('module'), [Symbol('export'), [
+      Symbol('fun'), 
+        Object.keys(env).filter(k => isNumber(env[k])).map(k => Symbol(k))
+      , flat_ast]]], env)
+    console.log(wat)
+    var wasm = wat2wasm(wat)
+    t.equal(wasm.apply(wasm,
+      Object.keys(env).filter(k => isNumber(env[k])).map(k => env[k])
+    ), values[i])
     t.end()
   })
 })

72dfa8db8f797e769d64411142fae4372d74df77
Dominic Tarr <dominic.tarr@gmail.com>
2020-04-07 19:31:37 +1200

    starting flatten tests over...

--- test/expression.js
+++ test/expression.js
@@ -1,5 +1,7 @@
 var tape = require('tape')
-var {flatten, stringify, isNumber} = require('../util')
+var {
+  flatten, stringify, isNumber, isExpressionTree
+} = require('../util')
 var parse = require('../parse')
 var ev = require('../eval')
 var compileWat = require('../compile/wat')
@@ -14,32 +16,60 @@ var env = {
 
 
 var inputs = [
-  '(add (if a 1 2) 3)',
   '(add 1 2)',
-  '(if (and a 1) 0 1)',
-  // (and a b) ;; except that then/else block is a statement so can hold another if
-  '(if a (if b 1 -1) -10)',
-    //(or d e f)
-  '(if d 1 (if e 10 (if f 100 -1) -10) -100)',
-  '[block (def n 1) {loop (lt n 100) (def n (add n n))}]'
+  '(if a 1 2)',
+  '(add (if a 3 2) 5)',
+  '(if a (if b 2 1) 0)', //(AND a b)
+//  '(add (if a 1 2) 3)',
+//  '(if (and a 1) 0 1)',
+//  // (and a b) ;; except that then/else block is a statement so can hold another if
+//  '(if a (if b 1 -1) -10)',
+//    //(or d e f)
+//  '(if d 1 (if e 10 (if f 100 -1) -10) -100)',
+//  '[block (def n 1) {loop (lt n 100) (def n (add n n))}]'
+]
+
+
+var isExprTree = [
+  true,
+  false,
+  false,
+  false
+//  false,
+//  false,
+//  false,
+//  false
 ]
 
 var outputs = [
-  '(block (if a (def $1 1) (def $1 2)) (def $2 (add $1 3)))',
-  '(block (def $1 (add 1 2)))',
-  '(block (def $1 (and a 1)) (block (if $1 (def $2 0) (def $2 1)) $2))',
-  '(block (if a (block (if b (def $2 100) (def $2 -1)) (def $1 $2)) (def $1 -1)) $1)',
-  '(block (def n 1) (loop (lt n 100) (def $1 (add n n)) $1)'
+ '(add 1 2)',
+ '(block (if a (def $1 1) (def $1 2)) $1)',
+ '(block (if a (def $2 3) (def $2 2)) (add $2 5))',
+ '(block (if a (if b (def $1 2) (def $1 1)) (def $1 0)) $1)',
+//  '(block (if a (def $1 1) (def $1 2)) (def $2 (add $1 3)))',
+//  '(block (def $1 (add 1 2)))',
+//  '(block (def $1 (and a 1)) (block (if $1 (def $2 0) (def $2 1)) $2))',
+//  '(block (if a (block (if b (def $2 100) (def $2 -1)) (def $1 $2)) (def $1 -1)) $1)',
+//  '(block (def n 1) (block (loop (lt n 100) (def $1 (add n n))) $1))'
 ]
 
 var values = [
-  4,
   3,
-  0,
+  1,
+  8,
+  1,
   -1,
   100,
   128
 ]
+
+tape('is expression tree', function (t) {
+  inputs.forEach(function (tree, i) {
+    t.equal(isExpressionTree(parse(tree)), isExprTree[i], stringify(tree) + ' should ' + (isExprTree[i] ? 'be' : 'not be') + ' an expression tree')
+  })
+  t.end()
+})
+
 
 /*
 ['pre', (def v (if a (set v 1) (set v 2))), (add v 3)
@@ -65,10 +95,11 @@ inputs.forEach(function (_, i) {
     console.log("FLAT", stringify(flat_ast))
     //disable checking exact output, because it's not perfect
     //but the important thing is it behaves correctly.
-    //t.equal(stringify(), outputs[i])
+    t.equal(stringify(flat_ast), outputs[i])
     t.equal(ev(ast, {__proto__: env}), values[i])
     console.log(stringify(flat_ast))
     t.equal(ev(flat_ast, {__proto__: env}), values[i])
+
     return t.end()
     var wat = compileWat([Symbol('module'), [Symbol('export'), [
       Symbol('fun'), 

cc936aa6ddfbe0e9186ded672a834246107997b5
Dominic Tarr <dominic.tarr@gmail.com>
2020-04-07 19:43:58 +1200

    amazing, much more tests, and it's all working

--- test/expression.js
+++ test/expression.js
@@ -14,31 +14,22 @@ var env = {
   lt: ([a, b]) => a < b
 }
 
-
 var inputs = [
   '(add 1 2)',
   '(if a 1 2)',
   '(add (if a 3 2) 5)',
   '(if a (if b 2 1) 0)', //(AND a b)
-//  '(add (if a 1 2) 3)',
-//  '(if (and a 1) 0 1)',
-//  // (and a b) ;; except that then/else block is a statement so can hold another if
-//  '(if a (if b 1 -1) -10)',
-//    //(or d e f)
-//  '(if d 1 (if e 10 (if f 100 -1) -10) -100)',
-//  '[block (def n 1) {loop (lt n 100) (def n (add n n))}]'
+  '(if d 1 (if e 1 0))', //(OR d e)
+  '(if d 1 (if e 1 (if f 1 0)))', //(OR d e)
 ]
-
 
 var isExprTree = [
   true,
   false,
   false,
+  false,
+  false,
   false
-//  false,
-//  false,
-//  false,
-//  false
 ]
 
 var outputs = [
@@ -46,11 +37,8 @@ var outputs = [
  '(block (if a (def $1 1) (def $1 2)) $1)',
  '(block (if a (def $2 3) (def $2 2)) (add $2 5))',
  '(block (if a (if b (def $1 2) (def $1 1)) (def $1 0)) $1)',
-//  '(block (if a (def $1 1) (def $1 2)) (def $2 (add $1 3)))',
-//  '(block (def $1 (add 1 2)))',
-//  '(block (def $1 (and a 1)) (block (if $1 (def $2 0) (def $2 1)) $2))',
-//  '(block (if a (block (if b (def $2 100) (def $2 -1)) (def $1 $2)) (def $1 -1)) $1)',
-//  '(block (def n 1) (block (loop (lt n 100) (def $1 (add n n))) $1))'
+ '(block (if d (def $1 1) (if e (def $1 1) (def $1 0))) $1)',
+ '(block (if d (def $1 1) (if e (def $1 1) (if f (def $1 1) (def $1 0)))) $1)',
 ]
 
 var values = [
@@ -58,7 +46,8 @@ var values = [
   1,
   8,
   1,
-  -1,
+  0,
+  1,
   100,
   128
 ]
@@ -70,41 +59,26 @@ tape('is expression tree', function (t) {
   t.end()
 })
 
-
-/*
-['pre', (def v (if a (set v 1) (set v 2))), (add v 3)
-
-
-(add (if a 1 2) (if b 3 4))
-
-=>
-
-
-(block
-  (def v1 (if a (set v1 1) (set v2 2)))
-  (def v2 (if b (set v2 3) (set v2 4)))
-  (dev v3 (add v1 v2))
-)
-*/
-
-  console.log(stringify(flatten(parse(inputs[0]))))
 inputs.forEach(function (_, i) {
   tape('flatten:'+inputs[i], function (t) {
     var ast = parse(inputs[i])
+    console.log("tree:", stringify(ast))
     var flat_ast = flatten(parse(inputs[i]))
-    console.log("FLAT", stringify(flat_ast))
-    //disable checking exact output, because it's not perfect
-    //but the important thing is it behaves correctly.
+    console.log("flat:", stringify(flat_ast))
+
+    //assert that the flattened tree is expected
     t.equal(stringify(flat_ast), outputs[i])
+
+    //assert that the evaluation result is the same
     t.equal(ev(ast, {__proto__: env}), values[i])
-    console.log(stringify(flat_ast))
     t.equal(ev(flat_ast, {__proto__: env}), values[i])
 
-    return t.end()
+    //compile to webassembly and check that's the same too.
     var wat = compileWat([Symbol('module'), [Symbol('export'), [
-      Symbol('fun'), 
+      Symbol('fun'),
         Object.keys(env).filter(k => isNumber(env[k])).map(k => Symbol(k))
       , flat_ast]]], env)
+
     console.log(wat)
     var wasm = wat2wasm(wat)
     t.equal(wasm.apply(wasm,

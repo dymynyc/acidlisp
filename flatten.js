88d460e3e0cb708e80c4c5a70b05bb9f987311f7
Dominic Tarr <dominic.tarr@gmail.com>
2020-04-09 14:52:15 +1200

    flatten store instructions

--- flatten.js
+++ flatten.js
@@ -12,7 +12,7 @@ var syms = require('./symbols')
  // and convert it to wasm statements. //
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
-//var DEF = syms.def, IF = Symbol('if'), BLOCK = Symbol('block'), LOOP = Symbol('loop')
+ //var DEF = syms.def, IF = Symbol('if'), BLOCK = Symbol('block'), LOOP = Symbol('loop')
 
 function last (ary) {
   return ary[ary.length-1]
@@ -46,6 +46,34 @@ function insertDef(tree, sym) {
   }
 }
 
+
+function defaultFlatten (tree, n) {
+  var block = [syms.block]
+  var _tree = [tree[0]]
+  tree.forEach((branch,i) => {
+    if(isExpressionTree(branch)) _tree[i] = branch
+    else {
+      var _branch = flatten(branch, n+i)
+      block.push(_branch)
+      //XXX use insertDef here?
+      if(isSymbol(last(_branch))) {
+        _tree[i] = _branch.pop()
+        if(_branch.length == 2) //[block, something]
+          block[block.length-1] = _branch[1]
+      }
+      else {
+        _branch[_branch.length-1] = [syms.def, $(n+i), last(_branch)]
+        _tree[i] = $(n+i)
+      }
+    }
+  })
+  block.push(_tree)
+  return block
+
+}
+
+//this is working for current tests, but I am sure it has bugs
+
 var flatten = module.exports = function flatten (tree, n) {
   n = n || 1
   if(isExpressionTree(tree)) return tree
@@ -56,9 +84,11 @@ var flatten = module.exports = function flatten (tree, n) {
       return [syms.block, [syms.if,
         tree[1], insertDef(tree[2], sym), insertDef(tree[3], sym)
       ], sym]
+    else
+      throw new Error('if with non-expression test not implemented yet')
   }
   else if(tree[0] === syms.block) {
-    var block = [syms.block]
+    var block = [syms.block], value
     for(var i = 1; i < tree.length; i++) {
       if(isExpressionTree(tree[i]))
         block.push(tree[i])
@@ -69,12 +99,11 @@ var flatten = module.exports = function flatten (tree, n) {
         //if we are not at the last block expression
         //we can dump the value symbol
         if(isSymbol(last(_tree)) && i + 1 < tree.length)
-          _tree.pop()
+          value = _tree.pop()
 
         //append the items into the same block
         for(var j = 1; j < _tree.length; j++)
           block.push(_tree[j])
-
       }
     }
     return block
@@ -89,27 +118,29 @@ var flatten = module.exports = function flatten (tree, n) {
         $(m)
       )], $(m)]
   }
-  else {
-    var block = [syms.block]
-    var _tree = [tree[0]]
-    tree.forEach((branch,i) => {
-      if(isExpressionTree(branch)) _tree[i] = branch
-      else {
-        var _branch = flatten(branch, n+i)
-        block.push(_branch)
-        //XXX use insertDef here?
-        if(isSymbol(last(_branch))) {
-          _tree[i] = _branch.pop()
-          if(_branch.length == 2) //[block, something]
-            block[block.length-1] = _branch[1]
-        }
-        else {
-          _branch[_branch.length-1] = [syms.def, $(n+i), last(_branch)]
-          _tree[i] = $(n+i)
-        }
+  else if (isSymbol(tree[0]) && /_store$/.test(tree[0].description)) {
+    //console.log("FLATTEN STORE", tree)
+//    if(isExpressionTree(tree[2]))
+//      [syms.block, [tree[1], tree[2], insertDef(tree[3], n)], 
+    var block = defaultFlatten(tree, n)
+  //  console.log('BLOCK', block)
+    if(syms.block === block[0]) {
+      var store = last(block)
+      if(isSymbol(last(store)))
+        block.push(last(store))
+      else {//must be an expression tree
+        var store = block.pop()
+        var value = store.pop()
+        var ref = $(++n)
+        block.push(insertDef(value, ref))
+        store.push(ref)
+        block.push(store)
+        block.push(ref)
       }
-    })
-    block.push(_tree)
+    }
+//    console.log("flattened", block)
     return block
   }
+  else
+    return defaultFlatten(tree, n)
 }

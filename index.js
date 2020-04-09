4abcd864b13ae23255535352c0f9cbe528b9284e
Dominic Tarr <dominic.tarr@gmail.com>
2020-04-09 16:55:58 +1200

    fix helpers

--- index.js
+++ index.js
@@ -4,20 +4,18 @@ var unroll   = require('./unroll')
 var wat      = require('./compile/wat')
 var wat2wasm = require('./wat2wasm')
 var js       = require('./compile/js')
+var {stringify} = require('./util')
 
+exports.js_eval = function (src) {
+  return eval(exports.js(src))
+}
 
 exports.js = function (src) {
-  throw new Error('not yet implemented')
+  return js(unroll(ev(parse(src), {})))
 }
 
 exports.wat = function (src) {
-  var v = src
-  console.log('parse ', v = parse(v))
-  console.log('eval  ', v = ev(v, {}))
-  console.log('unroll', v = unroll(v))
-  console.log('wat   ', v = wat(v))
-  return v
-  //return wat(unroll(ev(parse(src), {})))
+  return wat(unroll(ev(parse(src), {})))
 }
 
 exports.wasm = function (src) {

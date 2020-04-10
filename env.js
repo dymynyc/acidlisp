441e9c6eae3553d43688c4f7ccfae453021e8d96
Dominic Tarr <dominic.tarr@gmail.com>
2020-04-11 10:42:19 +1200

    some methods for working with lists

--- env.js
+++ env.js
@@ -11,3 +11,19 @@ exports.mul = function (args) {
 exports.div = function (args) {
   return args.reduce((a, b) => a / b)
 }
+
+exports.head = function ([list]) {
+  return list[0]
+}
+exports.tail = function ([list]) {
+  return list.slice(1)
+}
+exports.cat = function ([a,b]) {
+  return a.concat(b)
+}
+exports.list = function (args) {
+  return args
+}
+exports.isEmpty = function ([list]) {
+  return list.length === 0
+}

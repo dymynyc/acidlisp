bb467ac250b7b096acf4e8f193426f941bf96a9a
Dominic Tarr <dominic.tarr@gmail.com>
2020-04-12 09:08:57 +1200

    string comparison methods (uses buffers internally)

--- env.js
+++ env.js
@@ -11,7 +11,12 @@ exports.mul = function (args) {
 exports.div = function (args) {
   return args.reduce((a, b) => a / b)
 }
-
+exports.mod = function ([a, b]) {
+  return a % b
+}
+exports.pow = function ([a, b]) {
+  return Math.pow(a, b)
+}
 exports.head = function ([list]) {
   return list[0]
 }
@@ -26,4 +31,28 @@ exports.list = function (args) {
 }
 exports.isEmpty = function ([list]) {
   return list.length === 0
+}
+exports.concat = function (args) {
+  return Buffer.concat(args)
+}
+exports.slice = function ([buffer, start, end]) {
+  return buffer.slice(start, end)
+}
+exports.char = function ([buffer, index]) {
+  return buffer[index]
+}
+exports.compare = function ([a, b]) {
+  return a.compare(b)
+}
+exports.compareAt = function ([buffer, start, end, _buffer, _start, _end]){
+  return buffer.compare(_buffer, _start, _end, start, end)
+}
+exports.join = function ([list, sep]) {
+  var a = []
+  for(var i = 0; i < list.length; i++) {
+    a.push(list[i])
+    a.push(sep)
+  }
+  a.pop() //remove last seperator
+  return Buffer.concat(a)
 }

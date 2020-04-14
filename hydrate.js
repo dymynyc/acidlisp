5ccf0f39835be2ff999f851015170c3074f73ba0
Dominic Tarr <dominic.tarr@gmail.com>
2020-04-14 13:50:52 +1200

    convert literals into memory, for all runtimes/compilers

---
+++ hydrate.js
@@ -0,0 +1,27 @@
+var fs = require('fs')
+var path = require('path')
+var parse = require('./parse')
+var mem_ast = parse(fs.readFileSync(path.join(__dirname, 'lib', 'memory.l6'), 'utf8'))
+var ev = require('./eval')
+var wrap = require('./wrap')
+var {isBuffer, isArray} = require('./util')
+
+module.exports = function (ast, env) {
+  //initialize memory manager
+  var mem = wrap(ev(mem_ast, env), env)
+  var free = 0
+  ;(function each (ast) {
+    ast.forEach(function (v, i) {
+      if(isBuffer(v)) {
+        var ptr = mem.alloc(v.length + 4)
+        env.memory.writeUInt32LE(v.length, ptr)
+        v.copy(env.memory, 4+ptr)
+        ast[i] = ptr
+      }
+      else if(isArray(v))
+        each(v)
+    })
+  })(ast)
+
+  return ast
+}

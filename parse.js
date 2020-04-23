64334217d5c520c1f55e9e778b56aeaf312c496b
Dominic Tarr <dominic.tarr@gmail.com>
2020-04-24 09:30:42 +1200

    save line numbers on each token, for better errors

--- parse.js
+++ parse.js
@@ -4,91 +4,92 @@ var {
   RECURSE,GROUP,TEXT,EMPTY,EXPECT,EOF
 }  = require('stack-expression')
 
-//mandatory whitespace (includes ;; comments)
-var __ = /^(?:(?:\s)|(?:;;[^\n]*))+/
+module.exports = function (src, filename) {
+  if(!filename)
+    console.error('pass filename to enable informative stacktraces')
+  var lines = [0]
+  var chars = 0
+  for(var i = 0; i < src.length; i++) {
+    if(src[i] === '\n') lines.push(chars)
+    chars++
+  }
 
-//optional whitespace (includes ;; comments)
-var _ = /^(?:(?:\s)|(?:;;[^\n]*))*/
+  //mandatory whitespace (includes ;; comments)
+  var __ = /^(?:(?:\s)|(?:;;[^\n]*))+/
 
-var value = RECURSE ()
+  //optional whitespace (includes ;; comments)
+  var _ = /^(?:(?:\s)|(?:;;[^\n]*))*/
 
-var syms = require('./symbols')
-//note: json's value types already capture.
-var {number, boolean} = require('stack-expression/examples/json')
-var sym = TEXT(/^[a-zA-Z;_][a-zA-Z0-9_]*/, function (text) { return syms[text] || Symbol(text) })
+  var value = RECURSE ()
 
-var describe_easy = 'number, string, boolean, nil, symbol, or list'
-var describe_value = 'number, string, boolean, nil, symbol, list, pair, property chain, or quote'
+  var syms = require('./symbols')
+  //note: json's value types already capture.
+  var {number, boolean} = require('stack-expression/examples/json')
+  var sym = TEXT(/^[a-zA-Z;_][a-zA-Z0-9_]*/, function (text) { return syms[text] || Symbol(text) })
 
-var nil = TEXT(/^nil/, function () { return null })
-var contents = GROUP(MAYBE(JOIN(value, __)))
-var list_round = AND('(', _, contents,  _, EXPECT(')', ') or value'))
-var list_square = AND('[', _, contents,  _, EXPECT(']', '] or value'))
-var list_curly = AND('{', _, contents,  _, EXPECT('}', '} or value'))
+  var describe_easy = 'number, string, boolean, nil, symbol, or list'
+  var describe_value = 'number, string, boolean, nil, symbol, list, pair, property chain, or quote'
 
-function PREFIX (str, sym) {
-  return GROUP(AND(str, EXPECT(value, describe_value)), e => [sym, e[0]])
-}
+  function meta(fn) {
+    return function (group, start) {
+      if(fn) group = fn(group, start)
+      //could make this faster using binary search.
+      //I thought I could just remember the last
+      for(var i = 0; i < lines.length && lines[i+1] < start; i++);
+      group.meta = {start, line: i+1, column: start - lines[i] + 1, filename}
+      return group
+    }
+  }
 
-var list = OR(list_round, list_square, list_curly)
-var easy_value = OR(list, string, number, nil, boolean, sym)
+  function META (rules, map) {
+    return GROUP(rules, meta(map))
+  }
 
-var quote = PREFIX("&", syms.quote)
-var unquote = PREFIX("$", syms.unquote)
+  var nil = TEXT(/^nil/, function () { return null })
+  var contents = META(MAYBE(JOIN(value, __)))
+  var list_round = AND('(', _, contents,  _, EXPECT(')', ') or value'))
+  var list_square = AND('[', _, contents,  _, EXPECT(']', '] or value'))
+  var list_curly = AND('{', _, contents,  _, EXPECT('}', '} or value'))
 
-//morning thought: infix style?
-// (foo a b) -> a ~foo b
-// (bar (foo a b) c) -> a ~foo b ~bar c
-// second foo must be parens, you'd need parens anyway.
-// (bar c (foo a b)) -> c ~bar (foo a b)
+  function PREFIX (str, sym) {
+    return META(AND(str, EXPECT(value, describe_value)), e => [sym, e[0]])
+  }
 
-//The following is a performance hack!
-//the most obvious way to do this involved
-//too much back tracking and any slightly non-trivial
-//string was really slow to parse.
+  var list = OR(list_round, list_square, list_curly)
+  var easy_value = OR(list, string, number, nil, boolean, sym)
 
-var shortcuts = GROUP(AND(easy_value, OR(
-    AND(_, ':', _, EXPECT(easy_value, describe_easy)), //pair
-    GROUP(MORE(AND(_, '.', _, EXPECT(easy_value, describe_easy))), e => {
-      return [syms.get, null].concat(e)
-    }),
-    EMPTY
-  )), (e) => {
-    if(e.length === 1) return e[0]
-    else if(e[1][0] === syms.get)
-      return (e[1][1] = e[0]), e[1]
-    else
-      return [e[0]].concat(e[1])
-  })
+  var quote = PREFIX("&", syms.quote)
+  var unquote = PREFIX("$", syms.unquote)
 
-value(OR(quote, unquote, shortcuts))
+  //morning thought: infix style?
+  // (foo a b) -> a ~foo b
+  // (bar (foo a b) c) -> a ~foo b ~bar c
+  // second foo must be parens, you'd need parens anyway.
+  // (bar c (foo a b)) -> c ~bar (foo a b)
 
-/*
-//this is what it used to be like!
-//leaving this here because it's an instructive!
-//this was much more elegant, but way way too slow.
-//one example, that only had 15 lists
-//was taking 5 seconds to parse with this code.
-//with the fix it's 4 ms!
+  //The following is a performance hack!
+  //the most obvious way to do this involved
+  //too much back tracking and any slightly non-trivial
+  //string was really slow to parse.
 
-function PAIR (sep, name) {
-  return GROUP(AND(easy_value, _, sep, _, easy_value), e => (name ? [name] : []).concat(e))
-}
+  var shortcuts = META(AND(easy_value, OR(
+      AND(_, ':', _, EXPECT(easy_value, describe_easy)), //pair
+      META(MORE(AND(_, '.', _, EXPECT(easy_value, describe_easy))), e => {
+        return [syms.get, null].concat(e)
+      }),
+      EMPTY
+    )), (e) => {
+      if(e.length === 1) return e[0]
+      else if(e[1][0] === syms.get)
+        return (e[1][1] = e[0]), e[1]
+      else
+        return [e[0]].concat(e[1])
+    })
 
-function JOIN_MUST (separator, name) {
-  return GROUP(AND(easy_value, MORE(AND(_, separator, _, easy_value))), e => name ? [name].concat(e) : e)
-}
+  value(OR(quote, unquote, shortcuts))
 
-var pair = PAIR(':')
-var chain = JOIN_MUST('.', syms.get)
+  var root = AND(_, value, _, EOF)
 
-//just noticed this should be OR(pair...)
-value(pair, chain, quote, easy_value)
-*/
-
-var root = AND(_, value, _, EOF)
-
-module.exports = function (src) {
   return root(src, 0).groups[0]
 }
 

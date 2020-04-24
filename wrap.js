cbdadbdca402970041373936029707d686a2722d
Dominic Tarr <dominic.tarr@gmail.com>
2020-04-24 22:15:10 +1200

    wrap should expose memory, like wasm.js does

--- wrap.js
+++ wrap.js
@@ -9,13 +9,18 @@ function wrapFn (fun, env) {
   return function () {
     return ev.call(fun, [].slice.call(arguments), env)
   }
+  
 }
 
 exports = module.exports = function (tree, env) {
-  if(isFun(tree))
-    return wrapFn(tree, env)
+  if(isFun(tree)) {
+    var fn = wrapFn(tree, env)
+    fn.memory = env.memory
+    return fn
+  }
   var o = {}
   tree.forEach(pair => o[pair[0].description] = wrapFn(pair[1], env))
+  o.memory = env.memory
   return o
 }
 

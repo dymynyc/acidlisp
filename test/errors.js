10cffbd08c63a33b54df7c15729e483f2c3b8f61
Dominic Tarr <dominic.tarr@gmail.com>
2020-04-24 15:34:03 +1200

    error test shouldn't crash

--- test/errors.js
+++ test/errors.js
@@ -1,13 +1,29 @@
 
-var m = '(module (export (fun () [fatal 1])) )'
+var recursive = `((fun R (n)
+    (if (gte n 200) [fatal 3] (R (add n 1)))
+  ) 0)`
 
 var acid = require('../')
 
 var tape = require('tape')
 
 tape('throws', function (t) {
-  t.throws(function () {
-    acid.wasm(m)()
-  })
+  try {
+    acid.eval(`(block
+      ;; do line numbers work?
+      (fatal 1)
+    )`, null, "EVAL1")
+  } catch (err) { console.error(err) }
+
+  try {
+    acid.eval(recursive, null, "EVAL2")
+  } catch (err) { console.error(err) }
+
+  try {
+    acid.eval(`(block
+      ;; do line numbers work?
+      (fatal 1)
+    )`)
+  } catch (err) { console.error(err) }
   t.end()
 })

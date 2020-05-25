096b5967856b9757630237b1e87bf7ae3e65f2db
Dominic <@EMovhfIrFk4NihAKnRNhrfRaqIhBv1Wj8pTxJNgvCCY=.ed25519>
2020-05-25 17:37:55 +1200

    scope tests

--- test/scope.js
+++ test/scope.js
@@ -1,6 +1,6 @@
 var tape = require('tape')
 var acid = require('../')
-var {isArray, isSymbol, stringify} = require('../util')
+var {isArray, isSymbol, stringify, pretty} = require('../util')
 var scopify = require('../scopify')
 
 function copy (ast) {
@@ -34,8 +34,7 @@ var inputs = [
     (scope (def a 7) (def b 5))
     (def b (add 3 a))
    b
-  )`,
-
+  )`
 ]
 
 var outputs = [1, 2, 3, 4, 5]
@@ -58,4 +57,31 @@ tape('eval scope', function (t) {
     t.equal(acid.eval(ast, scope), outputs[i])
   }
   t.end()
+})
+
+
+tape('scope', function (t) {
+  //here is a real case that didn't work.
+  //the lone var at the end wasn't getting replaced.
+  var src =  `
+    (def foo
+      [scope
+        ;;start of free memory stored in free global
+        (def free (i32_load 0))
+        ;; never alloc data at 0, because it looks like a null pointer.
+        (if (eq 0 free) (set free 4) 0)
+        ;;move it forward, by the amount requested
+        ;;also store at memory location 0
+        (def _free (add free size))
+        (i32_store 0 _free)
+        (set_global 0 _free)
+        ;;return the old position
+        free
+      ])`
+    var ast = acid.parse(src)
+    scopify(ast, {}, 0)
+    console.log(pretty(ast))
+    t.notEqual(ast[1], ast[2][ast[2].length-1])
+  t.end()
+
 })
